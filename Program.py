0538bb1befa5094ace4de2f998d7d3a556199cdf
udo0oy <udo0oy@gmail.com>
2018-09-18 20:12:40 +0600

    divide and conquer

--- Program.py
+++ Program.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pprint
@@ -39,34 +41,61 @@ count=0                                   # For counting same input
 cellcount= 0                              # For counting which cell to use
 for i, val in enumerate(result,0): 
  for j, valu in enumerate(result,0): 
-  x=levenshtein(val,valu)
-  if x == 0 :
-   count=count+1
-   print('levenshtein ' + val + ' and ' +valu+ ' is :  '+str(x) )  
+  if val!=None and valu != None:   
+   x=levenshtein(val,valu)
+   if x == 0 :
+    count=count+1
+    print('levenshtein ' + val + ' and ' +valu+ ' is :  '+str(x) )  
    if count > 0:
-    cell_list1[j].value = ' '
-    cell_list2[j].value = ' '
+    cell_list1[j].value = None
+    cell_list2[j].value = None
  cell_list1[cellcount].value = val
  cell_list2[cellcount].value = count   
  count=0
  cellcount = cellcount + 1
 
 for i, val in enumerate(cell_list1,0):
-   if cell_list1[i].value != ' ':
+   if cell_list1[i].value != None:
      for j, val in enumerate(cell_list1,0):
-         if cell_list1[j].value == ' ':
+         if cell_list1[j].value == None:
              cell_list1[j].value = cell_list1[i].value
              cell_list2[j].value = cell_list2[i].value
-             cell_list1[i].value = ' '
-             cell_list2[i].value = ' '
+             cell_list1[i].value = None
+             cell_list2[i].value = None
 
 #pp.pprint(cell_list1)
 #pp.pprint(cell_list2)
 sh.update_cells(cell_list1) # Update in batch
 sh.update_cells(cell_list2) # Update in batch
-
-
-
-
-
+tempname = sh.range('D1:D500')
+tempcount= sh.range('E1:E500')
+cell1 = sh.col_values(2)
+cell2 = sh.col_values(3)
+cell_list1=cell1
+cell_list2=cell2
+count = 0
+for i, val in enumerate(cell_list1,0):
+ if val != None:
+  print("1st value "+val)   
+  for j ,valu in enumerate(cell_list1,0):
+   if valu !=None :
+    x=levenshtein(val,valu)
+    print(val+" with "+valu + " is "+ str(x)) 
+    if x < 4:
+     tempname[i].value=val
+     tempcount[i].value=valu
+     if x !=0:
+      val=None
+      valu=None
+      count=count+1
+ if x==0:
+  val=None
+  valu=None
+for i, val in enumerate(tempname,1):
+ print(tempname[i].value)
+ cell_list1[i].value=val
+for i, val in enumerate(tempcount,1): 
+ cell_list2[i].value=val 
+sh.update_cells(tempname) # Update in batch
+sh.update_cells(tempcount) # Update in batch
 
