a46ad195b9de621744f3fdd3cce0c172aed5f262
udo0oy <udo0oy@gmail.com>
2018-09-27 12:10:13 +0600

    update after gitignor

--- src/path/phonetics/__init__.py
+++
@@ -1,4 +0,0 @@
-from .soundex import soundex  # NOQA
-from .metaphone import metaphone  # NOQA
-from .metaphone import dmetaphone  # NOQA
-from .nysiis import nysiis  # NOQA
--- src/path/write_to_googlesheet.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import json
-
-class GooglesheetWritter():
-    print('finish5')
-
