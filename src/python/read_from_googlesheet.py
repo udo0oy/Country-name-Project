c4a68734cf6e64a804b95a4468acd72893ea7e2f
udo0oy <udo0oy@gmail.com>
2018-09-27 17:27:11 +0600

    deleting unnecessary files

--- src/python/read_from_googlesheet.py
+++ src/python/read_from_googlesheet.py
@@ -7,10 +7,10 @@ from oauth2client.service_account import ServiceAccountCredentials
 class GooglesheetReader():
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('/home/fahim/Downloads/Country name project/src/python/Country name Project-0e6407319613.json', scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('/home/fahim/Downloads/Country name project/src/conf/Country name Project-0e6407319613.json', scope)
     client = gspread.authorize(creds)
     sh = client.open('Country name project').sheet1
-    result = sh.col_values(6)
+    result = sh.col_values(1)
 
     outputfilename = '/home/fahim/Downloads/Country name project/temp/data_from_googlesheets.json'
     with open(outputfilename, 'w') as outfile:
